f1517cd8c6d9fbadfa64d8bbc3fc5a5405156d5b
Jonathan Chang <jonathanchang@Jonathans-MBP.fios-router.home>
2018-08-12 17:41:39 -0400

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose.js');
+var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -21,7 +21,8 @@ app.post('/todos', (req, res) => {
   });
 });
 
-
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
